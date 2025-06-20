715ad65f161fb8463c4e74c3257de1c4b844a3a3
WilliamEntwistle <a1821496@student.adelaide.edu.au>
2025-06-20 01:42:11 +0000

    [automatic commit] 2025-06-20 01:42:11 UTC 60.225.178.209

--- part1/queries.sql
+++ part1/queries.sql
@@ -3,5 +3,5 @@ Values
 ('alice123', 'alice@example.com', 'hashed123', 'owner')
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
 ('carol123', 'carol@example.com', 'hashed789', 'owner')
-('alice123', 'alice@example.com', 'hashed321', 'walker')
-('alice123', 'alice@example.com', 'hashed654', 'owner')
+('willwalker', 'alice@example.com', 'hashed321', 'walker')
+('brad234', 'alice@example.com', 'hashed654', 'owner')
