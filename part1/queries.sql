a1423797d6d110377e7070e824c51ae2180afb39
WilliamEntwistle <a1821496@student.adelaide.edu.au>
2025-06-20 01:41:31 +0000

    [automatic commit] 2025-06-20 01:41:31 UTC 60.225.178.209

--- part1/queries.sql
+++ part1/queries.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (username, email, password_hash, role)
 Values
 ('alice123', 'alice@example.com', 'hashed123', 'owner')
-('alice123', 'alice@example.com', 'hashed123', 'owner')
-('alice123', 'alice@example.com', 'hashed123', 'owner')
-('alice123', 'alice@example.com', 'hashed123', 'owner')
+('alice123', 'alice@example.com', 'hashed456', 'walker')
+('alice123', 'alice@example.com', 'hashed789', 'owner')
+('alice123', 'alice@example.com', 'hashed321', 'walker')
 ('alice123', 'alice@example.com', 'hashed123', 'owner')
