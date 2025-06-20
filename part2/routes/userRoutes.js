299c79ca0eedd8daad91a061a08bb08df1ccae39
WilliamEntwistle <a1821496@student.adelaide.edu.au>
2025-06-20 07:58:55 +0000

    [automatic commit] 2025-06-20 07:58:55 UTC ;; communications error to 208.67.222.222#53: timed out 60.225.178.209

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -99,7 +99,7 @@ router.get('/mydogs', async (req, res) => {
   try {
     const [dogs] = await db.query(`
       SELECT dog_id, name FROM Dogs WHERE owner_id = ?
-      `, [ownerId]);
+      `, [req.session.user.user_id]);
       res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'failed' });
