7f9d3570af0639b4a86fa91e80f67af53873f49f
WilliamEntwistle <a1821496@student.adelaide.edu.au>
2025-06-20 04:47:12 +0000

    [automatic commit] 2025-06-20 04:47:12 UTC 60.225.178.209

--- part2/app.js
+++ part2/app.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+// Session
+const session = require()
+
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
