d5ccfccf4414bbb798a7835760a7f0bf16790fbf
WilliamEntwistle <a1821496@student.adelaide.edu.au>
2025-06-20 01:46:57 +0000

    [automatic commit] 2025-06-20 01:46:57 UTC 60.225.178.209

--- part1/queries.sql
+++ part1/queries.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (username, email, password_hash, role)
 Values
-('alice123', 'alice@example.com', 'hashed123', 'owner')
-('bobwalker', 'bob@example.com', 'hashed456', 'walker')
-('carol123', 'carol@example.com', 'hashed789', 'owner')
-('willwalker', 'will@example.com', 'hashed321', 'walker')
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('willwalker', 'will@example.com', 'hashed321', 'walker'),
 ('brad234', 'brad@example.com', 'hashed654', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
-((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max, 'medium')
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
