b68176ec2d273765ed552b3613ac104a359b5949
WilliamEntwistle <a1821496@student.adelaide.edu.au>
2025-06-20 23:29:43 +0000

    [automatic commit] 2025-06-20 23:29:43 UTC 60.225.178.209

--- part1/queries.sql
+++ part1/queries.sql
@@ -28,7 +28,7 @@ FROM Dogs d
 Join Users u ON d.owner_id = u.user_id;
 
 -- Query /api/walkrequests/open
-SELECT w.requested_time, w.location, d.name AS dog_name, u.username AS owner_username
+SELECT w.request_id, w.requested_time, w.location, d.name AS dog_name, u.username AS owner_username
 FROM WalkRequests w
 JOIN Dogs d ON w.dog_id = d.dog_id
 JOIN Users u ON d.owner_id = u.user_id
