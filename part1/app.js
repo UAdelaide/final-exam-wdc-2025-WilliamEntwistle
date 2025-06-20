4d236388aa9af6815922941a7e66a241aeb06451
WilliamEntwistle <a1821496@student.adelaide.edu.au>
2025-06-20 02:49:03 +0000

    [automatic commit] 2025-06-20 02:49:03 UTC 60.225.178.209

--- part1/app.js
+++ part1/app.js
@@ -125,13 +125,13 @@ let db;
     `);
 
     await db.execute(`
-        INSERT INTO users (username, email, password_hash, role)
+        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
         VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('willwalker', 'will@example.com', 'hashed321', 'walker'),
-        ('brad234', 'brad@example.com', 'hashed654', 'owner')
+        ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Barney'), '2025-06-10 12:00:00', 60, 'Beach', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Buster'), '2025-06-10 10:45:00', 20, 'Dog Park', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Louis'), '2025-06-10 11:30:00', 40, 'Plaza', 'accepted')
       `);
     }
   } catch (err) {
--- part1/queries.sql
+++ part1/queries.sql
@@ -20,4 +20,4 @@ VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Barney'), '2025-06-10 12:00:00', 60, 'Beach', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Buster'), '2025-06-10 10:45:00', 20, 'Dog Park', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Louis'), '2025-06-10 11:30:00', 40, 'Plaza', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Louis'), '2025-06-10 11:30:00', 40, 'Plaza', 'accepted');
